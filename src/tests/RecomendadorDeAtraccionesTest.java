4a04c67ad5994f2853b8d509cc1b0443229fc894
MicaCardoso <43869165+MicaCardoso@users.noreply.github.com>
2019-05-22 15:18:54 -0300

    Termino de Test para Recomendador

--- src/tests/RecomendadorDeAtraccionesTest.java
+++ src/tests/RecomendadorDeAtraccionesTest.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 
 public class RecomendadorDeAtraccionesTest {
 
+	
+	
+	/*Usuario con Id 4 ( Elrond )*/
 	@Test
 	public void obtenerPrimerProductoAlSugerirAUsuarioConId4() {
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(4);
@@ -17,6 +20,65 @@ public class RecomendadorDeAtraccionesTest {
 		assertEquals("Bosque Negro", productosRecomendados.get(0).obtenerNombre() );
 	}
 	
+	@Test ( expected = Error.class )
+	public void obtenerSegundoProductoAlSugerirAUsuarioConId4() {
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(4);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Moria", productosRecomendados.get(1).obtenerNombre() );
+	}
+	
+	
+	
+	/*Usuario con Id 2 ( Gandalf )*/
+	@Test
+	public void obtenerPrimerProductoAlSugerirAUSuarioConId2 (){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(2);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Erebor", productosRecomendados.get(0).obtenerNombre() );
+	}
+	
+	@Test
+	public void obtenerSegundoProductoAlSugerirAUSuarioConId2 (){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(2);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Minas Tirith", productosRecomendados.get(1).obtenerNombre() );
+	}
+	
+	@Test
+	public void obtenerTercerProductoAlSugerirAUSuarioConId2 (){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(2);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Abismo de Helm", productosRecomendados.get(2).obtenerNombre() );
+	}
+	
+	
+	@Test
+	public void obtenerCuartoProductoAlSugerirAUSuarioConId2 (){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(2);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Lothloriem", productosRecomendados.get(3).obtenerNombre() );
+	}
+	
+	@Test
+	public void obtenerQuintoProductoAlSugerirAUSuarioConId2 (){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(2);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Mordor", productosRecomendados.get(4).obtenerNombre() );
+	}
+	
+	@Test
+	public void obtenerSextoProductoAlSugerirAUSuarioConId2 (){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(2);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Moria", productosRecomendados.get(5).obtenerNombre() );
+	}
+	
+	@Test
+	public void obtenerSeptimoProductoAlSugerirAUSuarioConId2 (){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(2);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Bosque Negro", productosRecomendados.get(6).obtenerNombre() );
+	}
 	
 	
 	/*Usuario con Id 5 ( Aragorn )*/
@@ -32,7 +94,7 @@ public class RecomendadorDeAtraccionesTest {
 	public void obtenerSegundoProductoAlSugerirAUsuarioConId5() {
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Mordor", productosRecomendados.get(1).obtenerNombre() );
+		assertEquals("Pack Avetura", productosRecomendados.get(1).obtenerNombre() );
 	}
 	
 	
@@ -40,39 +102,50 @@ public class RecomendadorDeAtraccionesTest {
 	public void obtenerTercerProductoAlSugerirAUsuarioConId5() {
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Erebor", productosRecomendados.get(2).obtenerNombre() );
+		assertEquals("Pack Paisaje", productosRecomendados.get(2).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerCuartoProductoAlSugerirAUsuarioConId5() {
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Moria", productosRecomendados.get(3).obtenerNombre() );
+		assertEquals("Mordor", productosRecomendados.get(3).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerQuintoProductoAlSugerirAUsuarioConId5() {
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Minas Tirith", productosRecomendados.get(4).obtenerNombre() );
+		assertEquals("Erebor", productosRecomendados.get(4).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerSextoProductoAlSugerirAUsuarioConId5() {
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Abismo de Helm", productosRecomendados.get(5).obtenerNombre() );
+		assertEquals("Moria", productosRecomendados.get(5).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerSeptimoProductoAlSugerirAUsuarioConId5() {
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Bosque Negro", productosRecomendados.get(6).obtenerNombre() );
+		assertEquals("Minas Tirith", productosRecomendados.get(6).obtenerNombre() );
 	}
 	
+	@Test
+	public void obtenerOctavoProductoAlSugerirAUsuarioConId5() {
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Abismo de Helm", productosRecomendados.get(7).obtenerNombre() );
+	}
 	
-	
+	@Test
+	public void obtenerNovenoProductoAlSugerirAUsuarioConId5() {
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(5);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Bosque Negro", productosRecomendados.get(8).obtenerNombre() );
+	}
 	
 	
 	
@@ -104,47 +177,57 @@ public class RecomendadorDeAtraccionesTest {
 	public void obtenerCuartoProductoAlSugerirAUsuarioConId3(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Mordor", productosRecomendados.get(3).obtenerNombre() );
+		assertEquals("Pack Aventura", productosRecomendados.get(3).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerQuintoProductoAlSugerirAUsuarioConId3(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Erebor", productosRecomendados.get(4).obtenerNombre() );
+		assertEquals("Pack Paisaje", productosRecomendados.get(4).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerSextoProductoAlSugerirAUsuarioConId3(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Moria", productosRecomendados.get(5).obtenerNombre() );
+		assertEquals("Mordor", productosRecomendados.get(5).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerSeptimoProductoAlSugerirAUsuarioConId3(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Minas Tirith", productosRecomendados.get(6).obtenerNombre() );
+		assertEquals("Erebor", productosRecomendados.get(6).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerOctavoProductoAlSugerirAUsuarioConId3(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Abismo de Helm", productosRecomendados.get(7).obtenerNombre() );
+		assertEquals("Moria", productosRecomendados.get(7).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerNovenoProductoAlSugerirAUsuarioConId3(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Bosque Negro", productosRecomendados.get(8).obtenerNombre() );
+		assertEquals("Minas Tirith", productosRecomendados.get(8).obtenerNombre() );
 	}
 	
+	@Test
+	public void obtenerDecimoProductoAlSugerirAUsuarioConId3(){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Abismo de Helm", productosRecomendados.get(9).obtenerNombre() );
+	}
 	
-	
-	
+	@Test
+	public void obtenerUndecimoProductoAlSugerirAUsuarioConId3(){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(3);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("Bosque Negro", productosRecomendados.get(10).obtenerNombre() );
+	}
 	
 	
 	
@@ -169,23 +252,29 @@ public class RecomendadorDeAtraccionesTest {
 	public void obtenerTercerProductoAlSugeriAUsuarioDeId1(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(1);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Minas Tirith", productosRecomendados.get(2).obtenerNombre() );
+		assertEquals("Pack Paisaje", productosRecomendados.get(2).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerCuartoProductoAlSugeriAUsuarioDeId1(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(1);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("Abismo de Helm", productosRecomendados.get(3).obtenerNombre() );
+		assertEquals("Minas Tirith", productosRecomendados.get(3).obtenerNombre() );
 	}
 	
 	@Test
 	public void obtenerQuintoProductoAlSugeriAUsuarioDeId1(){
 		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(1);
 		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
-		assertEquals("La Comarca", productosRecomendados.get(4).obtenerNombre() );
+		assertEquals("Abismo de Helm", productosRecomendados.get(4).obtenerNombre() );
 	}
 	
+	@Test
+	public void obtenerSextoProductoAlSugeriAUsuarioDeId1(){
+		RecomendadorDeAtracciones recomendador = new RecomendadorDeAtracciones(1);
+		ArrayList<Producto> productosRecomendados = recomendador.recomendar();
+		assertEquals("La Comarca", productosRecomendados.get(5).obtenerNombre() );
+	}
 	
 	
 	
